dd789c693779588f59441352f1bb5c197bff7f9a
Daniel Aldea <daniel@aldeacms.com>
2013-09-25 12:57:24 -0300

    Cambios en estructura

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 $(document).ready(function(){
 	$("#toolbar a").on("click",function(){
 		$("#toolbar a").removeClass("active");
-		$(this).addClass("active");
+		page = "file://pages/"+$(this).attr("rel")+".html";
+		
+		$.ajax({
+			type: 'GET',
+			url: page,
+			async:false,
+			dataType:'html',
+			success: function(data) { 
+				$("#content").html(data);
+				$(this).addClass("active");
+				return false;
+			}
+		});
+				
 	});
+	
+	 resizeContent();
+	 $(window).on('resize', function(){
+		resizeContent();
+	});
+	
+	$("#tabHome").click();
+});
 
+function resizeContent(){
 	var totalHeight = $(window).height();
 	$("#content").css("height",totalHeight-90+"px");
-});
+}
