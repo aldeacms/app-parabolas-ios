066edfed7e4ca4edd222d5726f8f93e75aa95bae
Daniel Aldea <daniel@aldeacms.com>
2013-09-26 08:29:04 -0300

    return false

--- app.js
+++ app.js
@@ -37,7 +37,9 @@ function onDeviceReady() {
 				$("a[target=_blank]").on("click",function(){
 					URL = $(this).attr("href");
 					window.open(encodeURI(URL), '_blank', 'location=yes');
+					return false;
 				});
+				
 				return false;
 			}
 		});
