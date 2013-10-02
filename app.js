e3871dd9df73233093cd6fb0a9eaf5844bc57ea8
Daniel Aldea <daniel@aldeacms.com>
2013-10-02 00:27:41 -0300

    bugfix

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 var iabRef = null;
 function onDeviceReady() {
-	$("#toolbar a").on("click",function(){
-		$("navbar a").removeClass("active");
+	$(".navbar a").on("click",function(){
+		$(".navbar a").removeClass("active");
 		tab = $(this).attr("rel");
 		page = "pages/"+tab+".html";
 		
